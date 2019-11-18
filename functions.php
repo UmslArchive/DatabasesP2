1757da0228f9745dd9adccead50914c8c0a4e075
Colby Ackerman <colby@thinkhappymedium.com>
2019-11-18 12:15:48 -0600

    cleanup

--- functions.php
+++ functions.php
@@ -1,85 +1,100 @@
 <?php
-    //Global connection variables
-    $servername = "localhost";
-    $db = "p2";
-    $username = "root";
-    $password = "root";
-    $conn;
 
+//Global connection variables
+$servername = "localhost";
+$db = "p2";
+$username = "root";
+$password = "root";
+$conn;
 
-    function connectToDatabase() {
-        global $conn, $servername, $db, $username, $password;
-        $conn = new mysqli($servername, $username, $password, $db);
-        
-        if ($conn->connect_errno) {
-            echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
-        }
+function connectToDatabase() {
+    global $conn, $servername, $db, $username, $password;
+    $conn = new mysqli($servername, $username, $password, $db);
+    
+    if ($conn->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
     }
+}
 
-    function fetchQuestionBank() {
+function fetchQuestionBank() {
 
-    }
+}
 
-    function fetchAssignmentQuestions() {
+function fetchAssignmentQuestions() {
 
-    }
+}
 
-    function login() {
-        connectToDatabase();
-        $_SESSION["user"] = $_POST['userid'];
-    }
+function login() {
+    global $conn;
+    connectToDatabase();
 
-    function logout() {
+    //Verify valid user ID
+    $sql = "select uid from users where uid = " . $_POST['userid'];
+    $result = $conn->query($sql);
+
+    //If the user id is not found, restart the session
+    if($result->num_rows == 0) {
+        echo "Invalid User ID";
         $_SESSION = array();
         session_destroy();
     }
+    else {
+        //This is probably unsecure
+        $_SESSION["user"] = $_POST['userid'];
+    }
+}
 
-    function fetchCourses() {
-        global $conn;
-        connectToDatabase();
-        $user = $_SESSION["user"];
+function logout() {
+    $_SESSION = array();
+    session_destroy();
+}
 
-        //Query a users courses
-        $sql = "select name from courses, users where courses.uid = " . $user;
-        $result = $conn->query($sql);
+function fetchCourses() {
+    global $conn;
+    connectToDatabase();
+    $user = $_SESSION["user"];
 
-        if(isset($_SESSION["selectedCourse"])) {
-            echo "<option selected=\"selected\">" . $_SESSION["selectedCourse"] . "</option>";
-        }
+    //Query a users courses
+    $sql = "select name from courses, users where courses.uid = " . $user;
+    $result = $conn->query($sql);
 
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                if($_SESSION["selectedCourse"] !== $row["name"]) {
-                    echo "<option value=\"". $row["name"] ."\">" . $row["name"] . "</option>";
-                }
+    if(isset($_SESSION["selectedCourse"])) {
+        echo "<option selected=\"selected\">" . $_SESSION["selectedCourse"] . "</option>";
+    }
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if($_SESSION["selectedCourse"] !== $row["name"]) {
+                echo "<option value=\"". $row["name"] ."\">" . $row["name"] . "</option>";
             }
-        } else {
-            echo "0 results";
         }
-        $conn->close();
+    } else {
+        echo "0 results";
     }
+    $conn->close();
+}
 
-    function fetchAssignments() {
-        global $conn;
-        connectToDatabase();
-        $user = $_SESSION["user"];
+function fetchAssignments() {
+    global $conn;
+    connectToDatabase();
+    $user = $_SESSION["user"];
 
-        //Query a users assignments
-    }
+    //Query a users assignments
+}
 
-    function selectAssignment() {
+function selectAssignment() {
 
-    }
+}
 
-    function addNewQuestion() {
+function addNewQuestion() {
 
-    }
+}
 
-    function addQuestionToAssignment() {
+function addQuestionToAssignment() {
 
-    }
+}
 
-    function removeQuestionFromAssignment() {
+function removeQuestionFromAssignment() {
 
-    }
+}
