c6cba5114c357f010d4dcb8c1ce029315e48423b
UnholySwiss <colbyackerman@gmail.com>
2019-11-17 22:39:54 -0600

    Started shelling the program

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+    function connectToDatabase() {
+
+    }
+
+    function fetchQuestionBank() {
+
+    }
+
+    function fetchAssignmentQuestions() {
+
+    }
+
+    function login() {
+
+    }
+
+    function logout() {
+
+    }
+
+    function selectAssignment() {
+
+    }
+
+    function addNewQuestion() {
+
+    }
+
+    function addQuestionToAssignment() {
+
+    }
+
+    function removeQuestionFromAssignment() {
+
+    }
